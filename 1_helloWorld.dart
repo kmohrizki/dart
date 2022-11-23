1f318e105b2bac32fb2ba62efc3ed2a019506633
@kmohrizki <kmohrizki@gmail.com>
2022-11-23 14:03:56 +0700

    add note helloWord

--- 1_helloWorld.dart
+++ 1_helloWorld.dart
@@ -4,6 +4,6 @@ void main (){
 
 /*
 note :
-  - main adalah sebuah fungsi yang dijalankan pertama kali, ibarat kita mau menuntut ilmu pasti harus ada media nya dulu. media di sini bisa seperti sekolahan, perpustakaan dll
-  - print adalah sebuah fungsi yang dapat menampilkan apa yang ada di dalamnya. kalau kita menuntut ilmu di sekolahan, print ini fungsinya seperti bulpoin. kita tinggal gerakin di atas kertas maka akan tampil hasilnya
+  - main adalah sebuah fungsi yang dijalankan pertama kali, ibarat kita mau menuntut ilmu pasti harus ada media nya dulu. media di sini bisa seperti sekolahan, perpustakaan dll.
+  - print adalah sebuah fungsi yang dapat menampilkan apa yang ada di dalamnya. kalau kita menuntut ilmu di sekolahan, print ini fungsinya seperti bulpoin. kita tinggal gerakin di atas kertas maka akan tampil hasilnya.
 */
