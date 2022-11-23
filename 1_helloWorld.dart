ab2a13305cf45457f9d0eec620ccd29537dc1e13
kmohrizki <118339296+kmohrizki@users.noreply.github.com>
2022-11-23 13:59:12 +0700

    add helloWorld

---
+++ 1_helloWorld.dart
@@ -0,0 +1,9 @@
+void main (){
+  print('Hello, world!');
+}
+
+/*
+note :
+  - main adalah sebuah fungsi yang dijalankan pertama kali, ibarat kita mau menuntut ilmu pasti harus ada media nya dulu. media di sini bisa seperti sekolahan, perpustakaan dll
+  - print adalah sebuah fungsi yang dapat menampilkan apa yang ada di dalamnya. kalau kita menuntut ilmu di sekolahan, print ini fungsinya seperti bulpoin. kita tinggal gerakin di atas kertas maka akan tampil hasilnya
+*/
